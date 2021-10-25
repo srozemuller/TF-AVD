40153c6e5e29a0ac29f11007f2d844b7c11d3186
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-10-25 11:13:52 +0200

    test

--- config.tf
+++ config.tf
@@ -110,7 +110,7 @@ resource "azurerm_shared_image_version" "sig_version" {
   image_name          = azurerm_shared_image.sig_def.name
   resource_group_name = azurerm_shared_image.sig_def.resource_group_name
   location            = azurerm_shared_image.sig_def.location
-  managed_image_id    = module.output.initVm
+  managed_image_id    = module.azurerm_resources.initVM
   
   target_region {
     name                   = azurerm_shared_image_gallery.sig.location
