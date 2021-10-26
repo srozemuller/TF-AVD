e9591aa0a4247055fb10280dd4f4058033b6659d
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-10-26 14:40:01 +0200

    added vm user login role

--- deploy-avd.tf
+++ deploy-avd.tf
@@ -278,16 +278,25 @@ SETTINGS
 PROTECTED_SETTINGS
 }
 
-data "azurerm_role_definition" "role" { # access an existing built-in role
-  name = "Desktop Virtualization User"
+
+
+data "azuread_group" "aad_group" {
+  name = var.aad_group_name
+}
+data "azurerm_role_definition" "vm_user_login" {
+  name = "Virtual Machine User Login"
+}
+resource "azurerm_role_assignment" "vm_user_role" {
+  scope              = azurerm_resource_group.rg-avd.id
+  role_definition_id = data.azurerm_role_definition.vm_user_login.id
+  principal_id       = data.azuread_group.aad_group.id
 }
 
-resource "azuread_group" "aad_group" {
-  display_name = var.aad_group_name
-  security_enabled = true
+data "azurerm_role_definition" "desktop_user" { 
+  name = "Desktop Virtualization User"
 }
-resource "azurerm_role_assignment" "role" {
+resource "azurerm_role_assignment" "desktop_role" {
   scope              = azurerm_virtual_desktop_application_group.desktopapp.id
-  role_definition_id = data.azurerm_role_definition.role.id
-  principal_id       = azuread_group.aad_group.id
+  role_definition_id = data.azurerm_role_definition.desktop_user.id
+  principal_id       = data.azuread_group.aad_group.id
 }
